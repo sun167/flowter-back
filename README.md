8c498080f9f432a9ba4d5bc15d9c6a7437cca804
Cao-Son TRAN <ctran2021@campus-eni.fr>
2023-05-22 16:39:17 +0200

    Initialiser le projet

---
+++ README.md
@@ -0,0 +1,12 @@
+PHP 8.1
+
+https://windows.php.net/download#php-8.1
+https://www.educative.io/answers/how-to-install-php-8-on-windows
+
+MySQL
+https://dev.mysql.com/downloads/installer/
+
+Symfony
+https://symfony.com/doc/current/setup.html
+
+ 
---
+++ src/Entity/User.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $first_name = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $last_name = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $phone = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $driver_license_check = null;
+
+    #[ORM\Column]
+    private ?bool $identity_check = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $is_driver = null;
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->id;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(?string $first_name): self
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName(?string $last_name): self
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function isDriverLicenseCheck(): ?bool
+    {
+        return $this->driver_license_check;
+    }
+
+    public function setDriverLicenseCheck(?bool $driver_license_check): self
+    {
+        $this->driver_license_check = $driver_license_check;
+
+        return $this;
+    }
+
+    public function isIdentityCheck(): ?bool
+    {
+        return $this->identity_check;
+    }
+
+    public function setIdentityCheck(bool $identity_check): self
+    {
+        $this->identity_check = $identity_check;
+
+        return $this;
+    }
+
+    public function isIsDriver(): ?bool
+    {
+        return $this->is_driver;
+    }
+
+    public function setIsDriver(?bool $is_driver): self
+    {
+        $this->is_driver = $is_driver;
+
+        return $this;
+    }
+}
