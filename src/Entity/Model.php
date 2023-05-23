a5d94b5a63c6dd18219cde8594f68789c7d02c49
Cao-Son TRAN <ctran2021@campus-eni.fr>
2023-05-23 12:20:03 +0200

    BO Model

---
+++ src/Entity/Model.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ModelRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ModelRepository::class)]
+class Model
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $label = null;
+
+    #[ORM\Column]
+    private ?int $nbSeats = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $horsePower = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getNbSeats(): ?int
+    {
+        return $this->nbSeats;
+    }
+
+    public function setNbSeats(int $nbSeats): self
+    {
+        $this->nbSeats = $nbSeats;
+
+        return $this;
+    }
+
+    public function getHorsePower(): ?int
+    {
+        return $this->horsePower;
+    }
+
+    public function setHorsePower(?int $horsePower): self
+    {
+        $this->horsePower = $horsePower;
+
+        return $this;
+    }
+}
