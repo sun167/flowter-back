2ba6b3207d6df3635cf36cd5fe412eb550515ed4
Cao-Son TRAN <ctran2021@campus-eni.fr>
2023-05-23 11:59:44 +0200

    BO Maintenance

---
+++ src/Entity/Maintenance.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MaintenanceRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MaintenanceRepository::class)]
+class Maintenance
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $commentary = null;
+
+    #[ORM\Column(type: Types::BLOB, nullable: true)]
+    private $document = null;
+
+    #[ORM\ManyToOne(inversedBy: 'maintenances')]
+    private ?User $user = null;
+
+    #[ORM\ManyToOne(inversedBy: 'maintenances')]
+    private ?Incident $incident = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getCommentary(): ?string
+    {
+        return $this->commentary;
+    }
+
+    public function setCommentary(?string $commentary): self
+    {
+        $this->commentary = $commentary;
+
+        return $this;
+    }
+
+    public function getDocument()
+    {
+        return $this->document;
+    }
+
+    public function setDocument($document): self
+    {
+        $this->document = $document;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getIncident(): ?Incident
+    {
+        return $this->incident;
+    }
+
+    public function setIncident(?Incident $incident): self
+    {
+        $this->incident = $incident;
+
+        return $this;
+    }
+}
