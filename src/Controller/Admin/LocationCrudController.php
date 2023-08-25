a58f5df539e1eb5818c106aa0082c7746e880aea
sun167 <caoson167@gmail.com>
2023-08-25 13:42:40 +0200

    API Location - enlève les champs dans la requete POST

--- src/Controller/Admin/LocationCrudController.php
+++ src/Controller/Admin/LocationCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Location;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class LocationCrudController extends AbstractCrudController
 {
@@ -12,14 +13,13 @@ class LocationCrudController extends AbstractCrudController
         return Location::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            TextField::new('adresse'),
         ];
     }
-    */
+    
 }
--- src/Entity/Location.php
+++ src/Entity/Location.php
@@ -3,43 +3,58 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
 use App\Repository\LocationRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
-#[ApiResource]
+#[ApiResource(normalizationContext: ['groups' => ['get']])]
+#[Get]
+#[Post(
+normalizationContext: ['groups' => ['postRead']],
+denormalizationContext: ['groups' => ['postWrite']]
+)]
 #[ORM\Entity(repositoryClass: LocationRepository::class)]
 class Location
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups('get')]
     private ?int $id = null;
 
+    #[Groups(['get', 'postWrite'])]
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $name = null;
 
+    #[Groups(['get', 'postWrite'])]
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $adresse = null;
 
-    #[ORM\OneToOne(inversedBy: 'location', cascade: ['persist', 'remove'])]
-    private ?Car $car = null;
-
+    #[Groups('get')]
     #[ORM\OneToMany(mappedBy: 'location', targetEntity: Ride::class)]
     private Collection $rides;
-
+    #[Groups('get')]
     #[ORM\OneToMany(mappedBy: 'departurePlace', targetEntity: Ride::class)]
     private Collection $ridesDepartFromThisLocation;
 
+    #[Groups('get')]
     #[ORM\OneToMany(mappedBy: 'destination', targetEntity: Ride::class)]
     private Collection $ridesComesToThisDestination;
+
+    #[Groups('get')]
+    #[ORM\OneToMany(mappedBy: 'location', targetEntity: Car::class)]
+    private Collection $cars;
 
     public function __construct()
     {
         $this->rides = new ArrayCollection();
         $this->ridesDepartFromThisLocation = new ArrayCollection();
         $this->ridesComesToThisDestination = new ArrayCollection();
+        $this->cars = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -67,18 +82,6 @@ class Location
     public function setAdresse(?string $adresse): self
     {
         $this->adresse = $adresse;
-
-        return $this;
-    }
-
-    public function getCar(): ?Car
-    {
-        return $this->car;
-    }
-
-    public function setCar(?Car $car): self
-    {
-        $this->car = $car;
 
         return $this;
     }
@@ -167,6 +170,36 @@ class Location
             // set the owning side to null (unless already changed)
             if ($ridesComesToThisDestination->getDestination() === $this) {
                 $ridesComesToThisDestination->setDestination(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Car>
+     */
+    public function getCars(): Collection
+    {
+        return $this->cars;
+    }
+
+    public function addCar(Car $car): static
+    {
+        if (!$this->cars->contains($car)) {
+            $this->cars->add($car);
+            $car->setLocation($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCar(Car $car): static
+    {
+        if ($this->cars->removeElement($car)) {
+            // set the owning side to null (unless already changed)
+            if ($car->getLocation() === $this) {
+                $car->setLocation(null);
             }
         }
 
